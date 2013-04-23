764d85e5f8541cbb6d76288af47f9d78bd0bb729
Richard Willis <willis.rh@gmail.com>
2013-04-23 15:09:25 +0200

    Inherits: adjusted mixin definition to use ES5 object methods

--- Inherits/src/Inherits.js
+++ Inherits/src/Inherits.js
@@ -4,23 +4,36 @@
  *
  * ECMAScript 5 compatibility table:
  * http://kangax.github.io/es5-compat-table/
+ *
+ * Useful links:
+ * https://developer.mozilla.org/en/docs/JavaScript/Reference/Global_Objects/Object/defineProperty
+ * https://developer.mozilla.org/en/docs/JavaScript/Reference/Global_Objects/Object/getOwnPropertyNames
+ * https://developer.mozilla.org/en/docs/JavaScript/Reference/Global_Objects/Object/getOwnPropertyDescriptor
+ *
+ * Basic usage:
+ * var Animal = function(){};
+ * var Cat = function();
+ * Cat.inherits(Animal);
+ * Cat.prototype.purr = function(){};
+ * var Dog = function(){};
+ * Dog.inherits(Animal, { bark: function() {} });
  */
 
 Object.defineProperty(Object.prototype, 'mixin', {
   value: function(obj) {
-    for (var prop in obj) {
-      if (obj.hasOwnProperty(prop)) {
-        this[prop] = obj[prop];
-      }
-    }
+    Object.getOwnPropertyNames(obj).forEach(function(prop) {
+      Object.defineProperty(this, prop, Object.getOwnPropertyDescriptor(obj, prop))
+    }.bind(this));
   }
 });
 
 Object.defineProperty(Function.prototype, 'inherits', {
-  value: function(_super, _subProto) {
+  value: function(_super, _mixin) {
     this.prototype = Object.create(_super.prototype);
     this.prototype.constructor = this;
-    this.prototype.mixin(_subProto);
+    if (_mixin instanceof Object) {
+      this.prototype.mixin(_mixin);
+    }
     return this;
   }
 });
