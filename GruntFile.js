ab9f4a8b19eded2e66a57743a077636261e9be98
Richard Willis <willis.rh@gmail.com>
2013-05-24 14:01:36 +0100

    Added GruntFile

---
+++ GruntFile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: [
+        '*.js',
+        '*/src/**/*.js',
+        '*/spec/**/*.js'
+      ],
+      options: {
+        globals: {
+          console: false,
+          document: false,
+          require: false
+        }
+      }
+    },
+    jasmine: {
+      src: [
+        '*/src/**/*.js',
+        '!Emailer/src/email.js',
+        '!ImageToBase64String/src/imageToBase64String.js'
+      ],
+      options: {
+        specs: '*/spec/**/*.js'
+      }
+    }
+  });
+
+  // Load the tasks
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+  // Register custom tasks
+  grunt.registerTask('lint', ['jshint']);
+  grunt.registerTask('test', ['lint', 'jasmine']);
+  grunt.registerTask('default', ['test']);
+};
