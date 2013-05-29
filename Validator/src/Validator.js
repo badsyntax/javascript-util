b2622c7dc529ba867bebac333592ec22167950bb
Richard Willis <willis.rh@gmail.com>
2013-05-29 22:02:22 +0100

    Added date rule test, and new module definition which now supports CommonJS + AMD

--- Validator/src/Validator.js
+++ Validator/src/Validator.js
@@ -9,17 +9,32 @@
  * validator.rule('email', 'isEmail', 'Email must be a valid email');
  * var errors = validator.check();
  */
-(function(exports) {
+(function (root, factory) {
+  if (typeof exports === 'object') {
+    // CommonJS
+    factory(exports);
+  } else if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(['exports'], factory);
+  } else {
+    // Browser globals
+    factory(root);
+  }
+}(this, function (exports) {
 
-  // Rule tests
-  var Valid = (function() {
+  // Default rules
+  var Rules = (function() {
     return {
       notEmpty: function(val) {
-        return [ null, false, '' ].indexOf(val) === -1;
+        return !!val;
       },
       isEmail: function(val) {
         // RFC822 - Based on code written by Ross Kendall - http://goo.gl/bNP6l
-        return (/^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/).test( val );
+        return (/^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/).test(val);
+      },
+      isDate: function(val) {
+        // ISO 8601 - http://goo.gl/11gxS
+        return (/^([\+\-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+\-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/).test(val);
       }
     };
   })();
@@ -32,15 +47,14 @@
 
   Validator.prototype.rule = function(key, tester, message) {
 
+    // Get the rule tester from the default rules
     if (typeof tester === 'string') {
-      tester = Valid[tester];
-      // Default tester not found
-      if (tester === undefined) {
+      if ((tester = Rules[tester]) === undefined) {
         return;
       }
     }
 
-    // Invalid custom test
+    // Invalid rule tester
     if (typeof tester !== 'function') {
       return;
     }
@@ -70,5 +84,4 @@
   };
 
   exports.Validator = Validator;
-
-}(typeof exports === 'object' ? exports : window));
+}));
