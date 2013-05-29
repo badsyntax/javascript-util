6ac071ce1e3c90b598e484b2866bb5c30258e9cf
Richard Willis <willis.rh@gmail.com>
2013-05-30 00:30:31 +0100

    Added JSDoc style comments

--- Validator/src/Validator.js
+++ Validator/src/Validator.js
@@ -23,32 +23,61 @@
   }
 }(this, function (exports) {
 
-  var Rules = (function () {
-    return {
+  "use strict";
+
+  /**
+   * Rule tests
+   */
+  var Tests = {
+      /**
+       * Checks if a value is empty. Can be any falsy value, eg: '', null, false
+       * @returns {Boolean}
+       */
       notEmpty: function (val) {
         return !!val;
       },
+      /**
+       * Checks if a value is a valid RFC822 email string.
+       * Created by Ross Kendall - http://goo.gl/bNP6l
+       * @returns {Boolean}
+       */
       isEmail: function (val) {
-        // RFC822 - Based on code written by Ross Kendall - http://goo.gl/bNP6l
         return (/^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/).test(val);
       },
+      /**
+       * Checks if a value is a valid ISO 8601 DateTime string.
+       * Created by Dean Thrasher - http://goo.gl/G2bT2
+       * @returns {Boolean}
+       */
       isDate: function (val) {
-        // ISO8601 - http://goo.gl/G2bT2
         return (/^([\+\-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+\-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/).test(val);
       }
-    };
-  })();
+  };
 
+  /**
+   * Validator
+   * @name Validator
+   * @constructor
+   */
   function Validator(data) {
     this.data = data || {};
     this.rules = [];
   }
 
+  /**
+   * Adds a new rule for a specified key to test against
+   * @name Validator#rule
+   * @function
+   * @param {string} key - The data key
+   * @param {string|function} tester - The tester key or function
+   * @param {string} message - The validation message
+   * @returns {null|Object}
+   */
   Validator.prototype.rule = function (key, tester, message) {
 
     // Get the rule tester from the default rules
     if (typeof tester === 'string') {
-      if ((tester = Rules[tester]) === undefined) {
+      if ((tester = Tests[tester]) === undefined) {
         return;
       }
     }
@@ -64,6 +93,12 @@
     });
   };
 
+  /**
+   * Runs the rule tests against the data set
+   * @name Validator#check
+   * @function
+   * @returns {null|Object}
+   */
   Validator.prototype.check = function () {
 
     var errors = null, i = 0, j = this.rules.length;
